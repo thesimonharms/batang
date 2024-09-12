f51bbf63538fd1d8c1c073dc11a539bae3a70ba6
Simon Harms <simon@simonharms.com>
2024-09-12 07:17:46 -0400

    Added comments

--- batang.py
+++ batang.py
@@ -1,8 +1,13 @@
 import math
 
 if __name__ == "__main__":
+    # Get user input
     days_since = int(input("How many days has it been since your last carton? : "))
     packs_remaining = int(input("How many packs do you have left? : "))
     cigarettes_remaining = int(input("How many cigarrettes are in your open pack? : ")) + (packs_remaining * 20)
-    average = math.ceil(cigarettes_remaining / days_since)
+
+    # Find average
+    average = math.ceil(batang / days_since)
+
+    # Show result to user
     print("\n\nYour average cigarettes per day is {}.".format(average))
